5d6fda959bdff8a27dbce8a0d9aeda7a7e786ece
Zien010162 <51741055+Zien010162@users.noreply.github.com>
2019-07-11 09:22:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,7 @@ def callback():
         sendText(user,'ก็....ดี')
     else :
         sendText(user,'ตอบไม่ได้มีปัญหาไรป่าว')
-
-    return '',200
+        return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
